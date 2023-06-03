beac52eb599fe09a88c2f324068db13f156f753b
catfishcafe <akmhobbs@gmail.com>
2023-06-03 09:05:27 -0400

    timer working

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,43 @@
-function hideStartDiv() {
+const hideStartDiv = function() {
     document.getElementById('start-game-prompt').style.display = 'none';
 }
 
-function showQDiv() {
+const showQDiv = function() {
     document.getElementById('game-questions').style.display = 'block';
 }
 
+//TIMER
+
+let setTime = function() {
+    secondsLeft = 60;
+    const timerInterval = setInterval(function() {
+    secondsLeft--;
+    if(secondsLeft === 59){
+        minute.textContent = 0;
+    }
+    second.textContent = secondsLeft;
+    if (document.getElementById('end-game-display').style.display === 'block'){
+        return;
+        } else if(second.value === 0){
+        return displayEndScreen()
+        }
+    if(secondsLeft < 10){
+        second.textContent = '0' + secondsLeft
+    }
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+    }
+  }, 1000);
+}
+
+//END TIMER
+
 document.getElementById('start-button').addEventListener('click', hideStartDiv);
 document.getElementById('start-button').addEventListener('click', showQDiv);
+document.getElementById('start-button').addEventListener('click', setTime);
 
-//❌Do I want to combine those two functions into one so that I only need one line to call it? I think yes but I don't wnat to rock the boat rn :/ also maybe not bcs I also need to add a function for that button to populate the answer slot
-
-//This gets the h2 element where each question will display
 const gameQText = document.getElementById('game-question');
 
-//empty variable that will be passed the information to display questions randomly
 let displayedQ = {
     gameQ: '',
     thisQAnswers: [],
@@ -52,7 +75,6 @@ const q4 = {
     thisQAnswers: ["I Am Not A Robot", "Digital Love", 'Robot Rock', 'Human After All']
 };
 
-//an array of all the question variables
 let allQA = [q1, q2, q3, q4];
 
 //shuffle function found at https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
@@ -69,21 +91,17 @@ function shuffle(array) {
 
 shuffle(allQA);
 
-//Now displayedQ has value of whichever q variable was shuffled to the front of the allQA array
-//❌ do I need to define a variable first?? I did this a while ago and I don't remember why I did that
 let x = 0;
 displayedQ = allQA[x];
 
-//gets the actual words of the question from displayedQ and displays them in the H2 text space 
 gameQText.textContent = displayedQ.gameQ;
 
-//all of the spans where the answers will go are grabbed and put in an array, and then that array is shuffled
-//❌I think I should put this all in one function but I don't know how :(
 const answerSlots = document.getElementsByClassName('answer-slot');
 ansSlotsArray = [answerSlots[0], answerSlots[1], answerSlots[2], answerSlots[3]]; 
-shuffle(ansSlotsArray);
 
-//loops through the shuffled array above and puts each answer option in one of the empty areas
+shuffle(ansSlotsArray);
+//important: the array of answers is NOT being shuffled, because the first answer in the array is always the correct one; instead, each answer is being assigned a random slot (A-D).
+
 for (i = 0; i < ansSlotsArray.length; i++){
     ansSlotsArray[i].textContent = displayedQ.thisQAnswers[i]
 };
@@ -150,7 +168,7 @@ let ansC = document.getElementById('ansC');
 let ansD = document.getElementById('ansD');
 let correctAnswer;
 
-function onAnswerButtonClick(answerId, inputButton) {
+const onAnswerButtonClick = function (answerId, inputButton) {
     showNextButton();
     correctAnswer = displayedQ.thisQAnswers[0];
     selectedAns = document.getElementById(answerId).textContent;
@@ -201,6 +219,9 @@ buttonB.addEventListener('click', function() {onAnswerButtonClick('ansB', button
 buttonC.addEventListener('click', function() {onAnswerButtonClick('ansC', buttonC)});
 buttonD.addEventListener('click', function() {onAnswerButtonClick('ansD', buttonD)});
 
+   let testValue = 50;
+buttonA.addEventListener('click', function(){testValue--; console.log(testValue)});
+
 //❌BELLOW ARE TEST BUTTONS, MAKE SURE TO DELETE ALL THIS, AND DELETE THE HTML AND CSS❌
 function hideStartTest(){document.getElementById('start-game-prompt').style.display = 'none';}
 document.getElementById('hideStart').addEventListener('click', hideStartTest);
@@ -214,16 +235,9 @@ document.getElementById('showStart').addEventListener('click', showStartTest);
 function showQTest(){document.getElementById('game-questions').style.display = 'block';}
 document.getElementById('showQ').addEventListener('click', showQTest);
 
-
 //❌The "high score" text in the header then needs to show the high score instead of the questions div or anything else
 
 //❌Need to add the timer, that might be the best next step
-
-//❌Or making the questions div, which will have a space for the question and separate buttons for the multiple choice, and a section at the bottom that will say either right or wrong depending on the chosen question
-
-//❌...which means an if statement that says if the correct button is pressed, then section at the bottom says "right". not sure it even needs to be hidden it just needs to be empty at first. one way or the other.
-
-//❌The questions and answers themselves need to be selected by an array, like "if(q1){div content = question 1 content}", and then both the question and the multiple choice buttons will be given the appropriate content.
 
 //❌end screen has to have some buttons I guess
 
@@ -231,8 +245,4 @@ document.getElementById('showQ').addEventListener('click', showQTest);
 
 //❌something something local storage something something
 
-//❌make sure you can define web api and know how to traverse the dom
-
 //❌write questions
-
-//❌while we're on the subject make sure you follow the correct steps to merge because I'm getting tired of these merge conflicts. I'm the only person using this :/
